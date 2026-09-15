c486c7cbe7a08a59e43a8abe57ed4a91cdc9a93f
victordodan <dodan_victor@yahoo.com>
2026-09-15 03:18:51 +0000

    Added missing shape cases   +extended file documentation Uncommented boundingBox tests

--- src/main/scala/shapes.scala
+++ src/main/scala/shapes.scala
@@ -1,7 +1,12 @@
 package edu.luc.cs.laufer.cs371.shapes
 
-/** data Shape = Rectangle(w, h) | Location(x, y, Shape) */
+/** data Shape = Rectangle(w, h) | Location(x, y, Shape) |
+         Ellpise(w, h)           | Group(Shape)
+*/
+
 enum Shape derives CanEqual:
   case Rectangle(width: Int, height: Int)
+  case Ellipse(width: Int, height: Int)
   case Location(x: Int, y: Int, shape: Shape)
+  case Group(shapes: Shape*)
   // TODO add missing cases (see test fixtures)
